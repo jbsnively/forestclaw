e5efe20655d291553da899e3702f727962a5e09b
Jonathan B. Snively <snivelyj@erau.edu>
2021-07-22 23:44:02 -0400

    quick updates for 2d test

--- conf2.sh
+++ conf2.sh
@@ -11,8 +11,8 @@ export FCLAW_BIN=$FCLAW/local/bin
     F77="mpif77" \
     CC="mpicc" \
     CXX="mpicxx" \
-    CFLAGS="-Wall -O3" \
-    CXXFLAGS="-O3 -Wall" \
+    CFLAGS="-Wall -O3 -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+    CXXFLAGS="-O3 -Wall -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
     FFLAGS="-fast -assume buffered_io" 
 #    LIBS="-lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
 #    --enable-clawpack \
