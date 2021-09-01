0e66cc940836abaab913337861e14d71d117ef1f
Jonathan B. Snively <snivelyj@erau.edu>
2021-09-01 00:33:31 -0400

    fast updates.

--- conf3.sh
+++ conf3.sh
@@ -11,10 +11,11 @@ export FCLAW_BIN=$FCLAW/local/bin
     F77="mpif77" \
     CC="mpicc" \
     CXX="mpicxx" \
-    CFLAGS="-Wall -O3 -DFCLAW2D_PATCHDIM=3 -DFCLAW2D_REFINEDIM=2" \
-    CXXFLAGS="-O3 -Wall -DFCLAW2D_PATCHDIM=3 -DFCLAW2D_REFINEDIM=2" \
-    FFLAGS="-O3" 
-#    FFLAGS="-fast -assume buffered_io" 
+    CFLAGS="-Wall -fast -DFCLAW2D_PATCHDIM=3 -DFCLAW2D_REFINEDIM=2" \
+    CXXFLAGS="-fast -Wall -DFCLAW2D_PATCHDIM=3 -DFCLAW2D_REFINEDIM=2" \
+    FCFLAGS="-fast -assume buffered_io" \
+    FFLAGS="-fast -assume buffered_io" 
+#    FFLAGS="-O3" 
 #    LIBS="-lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
 #    --enable-clawpack \
 
