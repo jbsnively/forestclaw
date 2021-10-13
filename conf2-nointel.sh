073c78b0f94018f35ec849aaa63c6cc0facdc3d7
Jonathan B. Snively <snivelyj@erau.edu>
2021-10-13 14:35:01 -0400

    Create conf2-nointel.sh

---
+++ conf2-nointel.sh
@@ -0,0 +1,21 @@
+export LIBS=""
+export FCLAW=/Users/snively/projects/forestclaw
+export FCLAW_BIN=$FCLAW/local/bin
+
+./configure \
+    --enable-mpi \
+    --enable-clawpack \
+    --enable-magic2d \
+    --disable-shared \
+    --without-blas \
+    F77="mpif77" \
+    CC="mpicc" \
+    CXX="mpicxx" \
+    CFLAGS="-Wall -O3 -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+    CXXFLAGS="-O3 -Wall -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+    FCFLAGS="-O3" \
+    FFLAGS="-O3"
+#    LIBS="-lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
+#    --enable-clawpack \
+
+
