9e51eddfb4fc5e04517f11941f86f4215194ffba
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-06 20:34:09 -0500

    conf-cmake.sh

---
+++ conf-cmake.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+home=/Users/snively
+
+# For now, only gcc/g++/gfortran seem to work.
+
+cmake configure    \
+    -DCMAKE_C_COMPILER=cc \
+    -DCMAKE_C_FLAGS="-O3 -Wall" \
+    -DCMAKE_CXX_COMPILER=c++ \
+    -DCMAKE_CXX_FLAGS="-O3 -Wall"\
+    -DCMAKE_Fortran_COMPILER=gfortran  \
+    -DCMAKE_Fortran_FLAGS="-O3 -Wall -Wno-unused-dummy-argument" \
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,-no_compact_unwind" \
+    -Dmpi=on \
+    -Dclawpack=on \
+    ../forestclaw
