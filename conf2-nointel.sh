f23d111e7790c23dacdf1c256aeffb1342977fa1
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-06 22:10:48 -0500

    Update conf2-nointel.sh

--- conf2-nointel.sh
+++ conf2-nointel.sh
@@ -11,8 +11,8 @@ export FCLAW_BIN=$FCLAW/local/bin
     F77="mpif77" \
     CC="mpicc" \
     CXX="mpicxx" \
-    CFLAGS="-Wall -O3 -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
-    CXXFLAGS="-O3 -Wall -DFCLAW2D_PATCHDIM=2 -DFCLAW2D_REFINEDIM=2" \
+    CFLAGS="-Wall -O3" \
+    CXXFLAGS="-O3 -Wall" \
     FCFLAGS="-O3" \
     FFLAGS="-O3"
 #    LIBS="-lmkl_intel_lp64 -lmkl_core -lmkl_sequential"
