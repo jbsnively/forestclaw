0f41647ae0b79ca09b00680c605e370a9b84b516
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-09 19:10:01 -0500

    cmake changes

--- conf-cmake-intel.sh
+++ conf-cmake-intel.sh
@@ -8,10 +8,10 @@ cmake configure    \
     -DCMAKE_C_COMPILER=icc \
     -DCMAKE_C_FLAGS="-O3 -Wall -std=c11" \
     -DCMAKE_CXX_COMPILER=icpc \
-    -DCMAKE_CXX_FLAGS="-O3 -Wall -std=c11"\
+    -DCMAKE_CXX_FLAGS="-O3 -Wall -std=c11" \
     -DCMAKE_Fortran_COMPILER=ifort  \
     -DCMAKE_Fortran_FLAGS="-O3 -assume buffered_io" \
-    -DCMAKE_EXE_LINKER_FLAGS="-Wl" \
+    -DCMAKE_EXE_LINKER_FLAGS="" \
     -Dmpi=on \
     -Dclawpack=on \
     ../forestclaw
