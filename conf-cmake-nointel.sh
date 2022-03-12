cb3681f2c830f017cb37ddcd569da0282b67b9a3
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-12 16:30:45 -0500

    Update conf-cmake-nointel.sh

--- conf-cmake-nointel.sh
+++ conf-cmake-nointel.sh
@@ -9,16 +9,14 @@ export CC=cc
 export CXX=c++
 export FCFLAGS="-O3"
 export FFLAGS="-O3"
-CMAKE_C_COMPILER=cc
-CMAKE_C_FLAGS="-O3 -Wall"
-CMAKE_CXX_COMPILER=c++
-CMAKE_CXX_FLAGS="-O3 -Wall"
-CMAKE_Fortran_COMPILER=gfortran
-CMAKE_Fortran_FLAGS="-O3 -Wall -Wno-unused-dummy-argument"
-CMAKE_EXE_LINKER_FLAGS="-Wl,-no_compact_unwind"
+export CMAKE_C_COMPILER=cc
+export CMAKE_C_FLAGS="-O3 -Wall"
+export CMAKE_CXX_COMPILER=c++
+export CMAKE_CXX_FLAGS="-O3 -Wall"
+export CMAKE_Fortran_COMPILER=gfortran
+export CMAKE_Fortran_FLAGS="-O3 -Wall -Wno-unused-dummy-argument"
+export CMAKE_EXE_LINKER_FLAGS="-Wl,-no_compact_unwind"
 
-
-# For now, only gcc/g++/gfortran seem to work.
 
 cmake configure    \
     -Dmpi=on \
