37763ef08de1e8141a31e0eb6c41450b789025c7
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-20 19:37:13 -0400

    Update conf-cmake-nointel.sh

--- conf-cmake-nointel.sh
+++ conf-cmake-nointel.sh
@@ -7,14 +7,16 @@ export FC=gfortran
 export F77=gfortran
 export CC=cc
 export CXX=c++
+export CFLAGS="-O3 -std=gnu11 -Wno-error=implicit-function-declaration"
+export CXXFLAGS="-O3 -Wno-error=implicit-function-declaration"
 export FCFLAGS="-O3"
 export FFLAGS="-O3"
 export LDFLAGS=' -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/'
 export LIBRARY_PATH=$LIBRARY_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
 export CMAKE_C_COMPILER=cc
-export CMAKE_C_FLAGS="-O3 -Wall"
+export CMAKE_C_FLAGS="-O3 -Wall -std=gnu11 -Wno-error=implicit-function-declaration"
 export CMAKE_CXX_COMPILER=c++
-export CMAKE_CXX_FLAGS="-O3 -Wall"
+export CMAKE_CXX_FLAGS="-O3 -Wall -Wno-error=implicit-function-declaration"
 export CMAKE_Fortran_COMPILER=gfortran
 export CMAKE_Fortran_FLAGS="-O3 -Wall -Wno-unused-dummy-argument"
 export CMAKE_EXE_LINKER_FLAGS="-Wl,-no_compact_unwind"
