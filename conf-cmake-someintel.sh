d0c89df5af9127fa18a0c187cc0b1e799cac4c7c
Jonathan B. Snively <snivelyj@erau.edu>
2022-03-20 20:02:04 -0400

    Create conf-cmake-someintel.sh

---
+++ conf-cmake-someintel.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+home=/Users/snively
+
+
+export FC=ifort
+export F77=ifort
+export CC=cc
+export CXX=c++
+export FCFLAGS="-O3"
+export FFLAGS="-O3"
+export LDFLAGS=' -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/'
+export LIBRARY_PATH=$LIBRARY_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
+export CMAKE_C_COMPILER=cc
+export CMAKE_C_FLAGS="-O3 -Wall"
+export CMAKE_CXX_COMPILER=c++
+export CMAKE_CXX_FLAGS="-O3 -Wall"
+export CMAKE_Fortran_COMPILER=ifort
+export CMAKE_Fortran_FLAGS="-O3 -Wall -assume buffered_io"
+export CMAKE_EXE_LINKER_FLAGS="-Wl,-no_compact_unwind"
+
+
+cmake configure    \
+    -Dmpi=on \
+    -Dclawpack=on \
+    ../forestclaw
